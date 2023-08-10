480bdbc2780c7c12d57061838ac04fe6eb31e206
alif-gunadi <alifgunadi1303@gmail.com>
2023-08-10 15:37:23 +0700

    membuat MQTT menerima topik tapi masih belum

--- index.js
+++ index.js
@@ -19,30 +19,49 @@ const influx = new Influx.InfluxDB({
   database: process.env.BUCKET, 
   schema: [
     {
-      measurement: 'measurement',
+      measurement: 'sensor',
       fields: {
-        kWh: Influx.FieldType.FLOAT,
-        power: Influx.FieldType.FLOAT,
-        ampere: Influx.FieldType.FLOAT,
-        voltage: Influx.FieldType.FLOAT,
+        v: Influx.FieldType.FLOAT,
+        i: Influx.FieldType.FLOAT,
+        kW: Influx.FieldType.STRING,
+        kVA: Influx.FieldType.STRING,
+        kWH: Influx.FieldType.STRING,
+        pf: Influx.FieldType.STRING,
+        vunbal: Influx.FieldType.FLOAT,
+        iunbal: Influx.FieldType.FLOAT,
       },
-      tags: ['panel', 'floor'],
-    },
-  ],
+      tags: ['status']
+    }
+  ]
 });
 
 function saveDataToInfluxDB(topic, payload) {
-  const { panel, floor, energy, power, ampere, voltage } = JSON.stringify(topic, payload);
-  influx
-    .writePoints([
-      {
-        measurement: 'measurement',
-        fields: { kWh: (energy, power, ampere, voltage) },
-        tags: { panel, floor },
-      },
-    ])
-    .then(() => console.log('Data tersimpan di InfluxDB'))
-    .catch((error) => console.log('Error menyimpan data:', error));
+  // const { v, i, kW, kVA, kWH, pf, vinabal, iunbal } = JSON.stringify(payload);
+  if (payload.v && payload.v[0] && payload.i && payload.i[0]) {
+    influx
+      .writePoints([
+        {
+          measurement: 'sensor',
+          // fields: { v, i, kW, kVA, kWH, pf, vinabal, iunbal },
+          // fields: { kWh: (energy, power, ampere, voltage) },
+          fields: {
+            v: payload.v[0],
+            i: payload.i[0],
+            kW: payload.kW,
+            KVAL: payload.KVAL,
+            kWH: payload.kWH,
+            pf: payload.pf,
+            vunbal: payload.vunbal,
+            iunbal: payload.iunbal,
+          },
+          tags: { status: topic.status },
+        },
+      ])
+      .then(() => console.log('Data tersimpan di InfluxDB'))
+      .catch((error) => console.log('Error menyimpan data:', error));
+    } else {
+      console.log(`${payload}: \n Payload tidak lengkap untuk penyimapan data: \n`, payload);
+    }
 };
 
 //MQTT Service
@@ -84,7 +103,7 @@ client.on('connect', () => {
 });
 
 client.on('message', (topic, payload) => {
-  console.log('MQTT Recieved Topic:', topic.toString());
+  console.log('MQTT Recieved Topic:', payload.toString());
   saveDataToInfluxDB(topic, payload);
   client.end();
 });
@@ -151,75 +170,75 @@ app.get('/api/total-usage-2023', (req, res) => {
 });
 
 //InfluxDB set-up
-const {InfluxDB, Point} = require('@influxdata/influxdb-client');
+// const {InfluxDB, Point} = require('@influxdata/influxdb-client');
 
-const token = process.env.INFLUXDB_TOKEN;
-const url = 'http://localhost:8086';
+// const token = process.env.INFLUXDB_TOKEN;
+// const url = 'http://localhost:8086';
 
-const influxDB = new InfluxDB({ url, token });
+// const influxDB = new InfluxDB({ url, token });
 
-const org = process.env.ORG;
-const bucket = process.env.BUCKET;
+// const org = process.env.ORG;
+// const bucket = process.env.BUCKET;
 
-const writeClient = influxDB.getWriteApi(org, bucket)
+// const writeClient = influxDB.getWriteApi(org, bucket)
 
-const point1 = new Point('watt')
-  .tag('data', 'v')
-  .floatField('value', 224.7);
-  console.log(point1);
+// const point1 = new Point('watt')
+//   .tag('data', 'v')
+//   .floatField('value', 224.7);
+//   console.log(point1);
 
-const point2 = new Point('watt')
-  .tag('data', 'v')
-  .floatField('value',  224.7);
-  console.log(point2);
+// const point2 = new Point('watt')
+//   .tag('data', 'v')
+//   .floatField('value',  224.7);
+//   console.log(point2);
 
-const point3 = new Point('watt')
-  .tag('data', 'v')
-  .floatField('value', 0);
-  console.log(point3);
+// const point3 = new Point('watt')
+//   .tag('data', 'v')
+//   .floatField('value', 0);
+//   console.log(point3);
 
-const point4 = new Point('watt')
-  .tag('data', 'v')
-  .floatField('value', 149.8);
-  console.log(point4);
+// const point4 = new Point('watt')
+//   .tag('data', 'v')
+//   .floatField('value', 149.8);
+//   console.log(point4);
 
-const point5 = new Point('watt')
-  .tag('data', 'i')
-  .floatField('value', 0);
-  console.log(point5);
+// const point5 = new Point('watt')
+//   .tag('data', 'i')
+//   .floatField('value', 0);
+//   console.log(point5);
 
-const point6 = new Point('watt')
-  .tag('data', 'i')
-  .floatField('value', 0.2);
-  console.log(point6);
+// const point6 = new Point('watt')
+//   .tag('data', 'i')
+//   .floatField('value', 0.2);
+//   console.log(point6);
 
-const point7 = new Point('watt')
-  .tag('data', 'i')
-  .floatField('value', 0.02);
-  console.log(point7);
+// const point7 = new Point('watt')
+//   .tag('data', 'i')
+//   .floatField('value', 0.02);
+//   console.log(point7);
 
-const point8 = new Point('watt')
-  .tag('data', 'i')
-  .floatField('value', 0.7);
-  console.log(point8);
+// const point8 = new Point('watt')
+//   .tag('data', 'i')
+//   .floatField('value', 0.7);
+//   console.log(point8);
 
 
-const queryApi = new InfluxDB({url, token}).getQueryApi(org);
+// const queryApi = new InfluxDB({url, token}).getQueryApi(org);
 
-const fluxQuery = 'from(bucket:"Weist") |> range(start: -1) |> filter(fn: (r) => r._measurement == "watt")';
+// const fluxQuery = 'from(bucket:"Weist") |> range(start: -1) |> filter(fn: (r) => r._measurement == "watt")';
 
-const myQuery = async () => {
-  for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
-    const o = tableMeta.toObject(values)
-    console.log(
-      `${o._time} ${o._measurement} in: ${o._field}=${o._value}`
-    )
-  }
-};
+// const myQuery = async () => {
+//   for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
+//     const o = tableMeta.toObject(values)
+//     console.log(
+//       `${o._time} ${o._measurement} in: ${o._field}=${o._value}`
+//     )
+//   }
+// };
 
-myQuery();
+// myQuery();
 
-writeClient.writePoint(point1, point2);
-writeClient.close().then(() => {
-  console.log('Write finished');
-});
+// writeClient.writePoint(point1, point2);
+// writeClient.close().then(() => {
+//   console.log('Write finished');
+// });
